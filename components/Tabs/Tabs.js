b161b0da59399365c50c842028eae658b5570862
droneal11015 <droneal11015@gmail.com>
2019-04-19 12:00:36 -0400

    minor changes in code, still not working properly

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,14 +14,14 @@ class TabLink {
       this.cards = document.querySelectorAll('.card') ;
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelectorAll('.card[data-tab="${this.tabData}"]') ;
+      this.cards = document.querySelectorAll('.card[data-tab="${this.tabData}"]');
     }
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(this.cards).map( card => new TabCard(card));
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
-    this.tabElement.addEventListener('click', () => this.selectTab());
+    this.tabElement.addEventListener('click', () => {this.selectTab()});
   }
 
   selectTab(){
@@ -36,7 +36,7 @@ class TabLink {
     const cards = document.querySelectorAll('.card') ;
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach( card => card.style.display = 'none');
+    cards.forEach(card => card.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
     this.tabElement.classList.add('.active-tab');
